392c6c18fb7f8b02020e7d8345408833e5b35e37
Prerak <prerakl123@gmail.com>
2023-10-08 17:07:00 +0530

    Added media paths.

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,5 @@ import os
 
 HAARCASCADE_FILE_PATH = os.path.join('../face_detection', 'haarcascade_frontalface_default.xml')
 ATTENDANCE_DB_PATH = os.path.join('../database', 'attendance.db')
+IMAGES_DIR_PATH = os.path.join('../images')
+VIDEO_CLIPS_DIR_PATH = os.path.join('../video_clips')
